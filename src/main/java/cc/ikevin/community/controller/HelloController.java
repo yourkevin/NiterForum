87a34f4ef294d449d6533be0e7c2928513c8b814
kevin <kuang099@qq.com>
2019-07-12 17:43:53 +0800

    init repo

---
+++ src/main/java/cc/ikevin/community/controller/HelloController.java
@@ -0,0 +1,16 @@
+package cc.ikevin.community.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(@RequestParam(name = "name") String name, Model model) {
+        model.addAttribute("name",name);
+        return "hello";
+    }
+}
